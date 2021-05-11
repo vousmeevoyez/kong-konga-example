67846350b6a4c5cd85e4fe5d80fffaa6d1702ee2
Lucas Santos <lucassm02@gmail.com>
2021-05-11 17:30:11 -0300

    feat: add konga-net creation

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker network create kong-net
+docker network create konga-net
 docker-compose down --rmi local
 docker-compose up --build -d
 docker-compose -f docker-compose.production.yml up -d kong
