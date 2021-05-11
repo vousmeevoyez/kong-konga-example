4e4d84405be5be40450b14ffebd6b091c22d4721
Lucas Santos <lucassm02@gmail.com>
2021-05-10 21:11:47 -0300

    feat: add script to start composition

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 docker network create kong-net
-docker-compose up --build -d db kong-migrations
-sleep 10
-docker-compose up --build -d kong konga
+docker-compose down --rmi local
+docker-compose up --build
+
